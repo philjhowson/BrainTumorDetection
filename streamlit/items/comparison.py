7a232f5faa04de413ddd650cd7ba12da7a232887
philjhowson <philjhowson@outlook.com>
2025-02-01 00:23:36 +0100

    update streamlit

--- streamlit/items/comparison.py
+++ streamlit/items/comparison.py
@@ -17,4 +17,30 @@ def comparison():
         st.image(img, caption = """ROC-AUC and F1 Scores for each of the
                  models. The ROC-AUC score is in blue and the F1-Score
                  is in purple.
-                 """, use_container_width = True)    
+                 """, use_container_width = True)
+
+    st.markdown("""Overall, there were similar levels of performance for
+                each of the models, although the custom model scored
+                noticably lower on the ROC-AUC metric. This may be in
+                part due to the fact that both Resnet and DenseNet were
+                trained on a large set of images prior to being trained
+                on the MRI images. This was also reflected in the fact that
+                the custom model completed significantly more epochs before
+                early stopping was triggered. With further improvements
+                to the model architecture and finer hyper parameter tuning,
+                a better score could likely be achieved. However, the model
+                takes much longer to train because it requires more
+                trainable parameters and because it has to be trained
+                directly from weight initialization and cannot benefit from
+                transfer learning.
+                """)
+
+    st.markdown("""DenseNet was the fastest model to train, partially due
+                to the smaller number of parameters, both trainable and
+                overall, but also due to the fact that it was able to
+                benefit from transfer learning. The model is also significantly
+                more lightweight compared to ResNet (49 MB vs. 359 MB), and
+                while ResNet had the same advantages with transfer learning,
+                DenseNet still outperformed it. With more hyperparameter
+                tuning, it could also likely perform even better and within
+                a short period of time.""")
